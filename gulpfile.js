87f8b1775012a84f22cf4c2a9edd68f879c8f5f1
Justin Barber <barber.justin@gmail.com>
2016-08-09 04:56:01 -0400

    Babelify than Concat/Minify
    Seperate Babelify from concat/minify so we don't pass angular through
    babel and get warning. Using the already minified angular reduced size
    even further

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,16 @@ var gulp = require('gulp'),
   uglify = require('gulp-uglify'),
   babel = require('gulp-babel');
 
-gulp.task('js', function () {
-  gulp.src(['vendor/**/*', 'src/**/module.es6', 'src/**/*']).
+gulp.task('es6', function () {
+  gulp.src(['src/**/module.es6', 'src/**/*']).
     pipe(babel({
       presets: ['es2015']
     })).
+    pipe(gulp.dest('build/.'));
+});
+
+gulp.task('js', ['es6'], function () {
+  gulp.src(['vendor/**/*', 'build/**/module.js', 'src/**/*.js']).
     pipe(concat('app.js')).
     pipe(uglify()).
     pipe(gulp.dest('.'));
