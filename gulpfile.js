4ab47807aa1109e45d32b58e1a7c279b95787bf8
Justin Barber <barber.justin@gmail.com>
2016-08-09 10:25:23 -0400

    trying to debug why seems to leave module out when broken up into peices

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,34 @@
 var gulp = require('gulp'),
   concat = require('gulp-concat'),
   uglify = require('gulp-uglify'),
+  debug = require('gulp-debug'),
   babel = require('gulp-babel');
+
+var vendorFiles = [
+  'node_modules/angular/angular.min.js'
+]
+
+gulp.task('create_vendor', function () {
+  gulp.src(vendorFiles).
+    pipe(debug({title: 'vendor:' })).
+    pipe(concat('vendor.js')).
+    pipe(gulp.dest('build/.'));
+});
 
 gulp.task('es6', function () {
   gulp.src(['src/**/module.es6', 'src/**/*']).
+    pipe(debug({title: 'es6:' })).
     pipe(babel({
       presets: ['es2015']
     })).
     pipe(gulp.dest('build/.'));
 });
 
-gulp.task('js', ['es6'], function () {
-  gulp.src(['vendor/**/*', 'build/**/module.js', 'build/**/*.js']).
+gulp.task('js', ['es6', 'create_vendor'], function () {
+  gulp.src(['build/vendor.js', 'build/module.js', 'build/**/*.js']).
+    pipe(debug({title: 'js:' })).
     pipe(concat('app.js')).
+    pipe(debug({title: 'min:' })).
     pipe(uglify()).
     pipe(gulp.dest('.'));
 });
